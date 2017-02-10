f92a9e0b677f4927db84b721fb799e5acc28faca
HamzaAnis <hamzaanis9514@gmail.com>
2017-02-10 17:53:24 +0500

    First Commit

---
+++ HomeTaskWebProgramming/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace HomeTaskWebProgramming.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ HomeTaskWebProgramming/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeTaskWebProgramming.Startup))]
+namespace HomeTaskWebProgramming
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
